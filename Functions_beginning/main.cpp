5ac93636938c1ca39e2c8d8e2bc767b5eed25f0a
Lugov_rr81@top-academy.ru <Lugov_rr81@top-academy.ru>
2023-02-23 21:29:53 +0300

    Add project files.

---
+++ Functions_beginning/main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int Sum(int a, int b);            //Прототип функции (объявление функции)
+int Difference(int a, int b);
+int Product(int a, int b);
+int Quotient(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "Введите два числа: "; cin >> a >> b;
+	int c = Sum(a, b);            //Использование (вызов) функции
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Difference(a, b) << endl;
+	cout << a << " * " << b << " = " << Product(a, b) << endl;
+	cout << a << " / " << b << " = " << Quotient(a, b) << endl;
+}
+
+int Sum(int a, int b)             //Реализация функции (определение функции)
+{
+	int c = a + b;
+	return c;
+}
+int Difference(int a, int b)
+{
+	return a - b;
+}
+int Product(int a, int b)
+{
+	return a * b;
+}
+int Quotient(int a, int b)
+{
+	return a / b;
+}
